6d431e3fe35eb9ae38ae1e7d003c2ab765592ab2
Dickson <dicksonparlma@gmail.com>
2019-02-15 09:34:02 +0300

    Comments on U.I and Business logic

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
-/*USER INTERFACE LOGIC*/
 function  tracker(){
+  /*USER INTERFACE LOGIC*/
+
   var collect1=parseInt(document.getElementById("sideA").value);
   var collect2=parseInt(document.getElementById("sideB").value);
   var collect3=parseInt(document.getElementById("sideC").value);
   var sides=[collect1,collect2,collect3];
+  
+  /*BUSINESS LOGIC*/
+
 }
