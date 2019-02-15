a1a977f17ac5c371a0da705eb1387c4cf470f7ce
Dickson <dicksonparlma@gmail.com>
2019-02-15 10:57:16 +0300

    A check condition for isosceles triangle

--- scripts.js
+++ scripts.js
@@ -16,4 +16,6 @@ function  tracker(){
     var secondOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make an equilateral triangle";
     document.getElementById("trackOutput").innerHTML=secondOutput;
   }
+  else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
+  }
 }
