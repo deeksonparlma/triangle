7bd531f87bc5f72a35f2065915bc5fe110a40818
Dickson <dicksonparlma@gmail.com>
2019-02-15 11:06:14 +0300

    Output to display a triangle is isosceles on HTML

--- scripts.js
+++ scripts.js
@@ -21,6 +21,7 @@ function  tracker(){
     document.getElementById("trackOutput").innerHTML=thirdOutput;
   }
   else if(sides[0]!=sides[1] && sides[0]!=sides[2] && sides[2]!=sides[1]){
-    var thirdOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make a scalene triangle";
+    var fourthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make a scalene triangle";
+    document.getElementById("trackOutput").innerHTML=fourthOutput;
   }
 }
