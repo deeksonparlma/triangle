f242d087807a474fd5f02d94a71acf39fa9bde5a
Dickson <dicksonparlma@gmail.com>
2019-02-15 09:26:58 +0300

    user interface logic added

---
+++ scripts.js
@@ -0,0 +1,6 @@
+/*USER INTERFACE LOGIC*/
+function  tracker(){
+  var collect1=parseInt(document.getElementById("sideA").value);
+  var collect2=parseInt(document.getElementById("sideB").value);
+  var collect3=parseInt(document.getElementById("sideC").value);
+}
