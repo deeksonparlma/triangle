a294c6ca100c2ddb5d8a080dcec1bcfd1d6cbefa
Dickson <dicksonparlma@gmail.com>
2019-02-15 17:34:54 +0300

    final

--- scripts.js
+++ scripts.js
@@ -1,31 +1,39 @@
 function  tracker(){
   /*USER INTERFACE LOGIC*/
 
-  var collect1=parseInt(document.getElementById("sideA").value);
-  var collect2=parseInt(document.getElementById("sideB").value);
-  var collect3=parseInt(document.getElementById("sideC").value);
-  var sides=[collect1,collect2,collect3];
+  var collect1=document.getElementById("sideA").value;
+  var collect2=document.getElementById("sideB").value;
+  var collect3=document.getElementById("sideC").value;
+  var sidea=parseInt(collect1);
+  var sideb=parseInt(collect2);
+  var sidec=parseInt(collect3);
+  var sides=[sidea,sideb,sidec];
+  var type=["makes an equilateral triangle","makes an isosceles triangle","makes an scalene triangle","doesn't make a triangle"]
 
   /*BUSINESS LOGIC*/
 
-  if(sides[0]<=0 || sides[1]<=0 || sides[2]<=0){
-    var firstOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"can't make a triangle";
+  if(sides[0] <=0 || sides[1] <=0 || sides[2] <=0){
+    var firstOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
     document.getElementById("trackOutput").innerHTML=firstOutput;
   }
   else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){
-    var secondOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make an equilateral triangle";
+    var secondOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[0];
     document.getElementById("trackOutput").innerHTML=secondOutput;
   }
   else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
-    var thirdOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make an isosceles triangle";
+    var thirdOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[1];
     document.getElementById("trackOutput").innerHTML=thirdOutput;
   }
-  else if(sides[0]!=sides[1] && sides[0]!=sides[2] && sides[2]!=sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
-    var fourthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"make a scalene triangle";
+  else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[2]!==sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
+    var fourthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[2];
     document.getElementById("trackOutput").innerHTML=fourthOutput;
   }
-  else if(sides[0]+sides[1]<sides[2] && sides[1]+sides[2]<sides[0] && sides[2]+sides[0]<sides[1]){
-    var fifthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+"don't make a triangle";
-    document.getElementById("trackOutput").innerHTML=fifthOutput;  
+  else if(sides[0]+sides[1]<sides[2] || sides[1]+sides[2]<sides[0] && sides[2]+sides[0]<sides[1]){
+    var fifthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
+    document.getElementById("trackOutput").innerHTML=fifthOutput;
+  }
+  else {
+    var sixthOutput=type[3];
+    document.getElementById("trackOutput").innerHTML=sixthOutput;
   }
 }
