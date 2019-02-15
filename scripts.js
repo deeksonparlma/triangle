6cb4653c4883ef83736e398b9789c6484dcdbe88
Dickson <dicksonparlma@gmail.com>
2019-02-15 09:29:48 +0300

    Array to store user data

--- scripts.js
+++ scripts.js
@@ -3,4 +3,5 @@ function  tracker(){
   var collect1=parseInt(document.getElementById("sideA").value);
   var collect2=parseInt(document.getElementById("sideB").value);
   var collect3=parseInt(document.getElementById("sideC").value);
+  var sides=[collect1,collect2,collect3];
 }
